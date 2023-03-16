3165ad42b93dbd29e6d7c2cbfbec04cb467f6c72
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-03-16 22:25:37 +0800

    Update for basic testing phase

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#include "../parser/tokenizer.h"
 
 int main() {
-    
+
+    /**
+     * Basic testing phase
+     */
+    Interpreter interp;
+
+    interp.input = "INT a = 5;";
+    interp.input_size = strlen(interp.input);
+    interp.pos = 4;
+    interp.line = 1;
+
+    skip_whitespace(&interp);
+    read_identifier(&interp);
+
+    // This should print 35 a 5 1 10
+    // 35 is TOKEN_IDENTIFIER
+    printf("%d %s %d %d %d" , interp.current_token.type, interp.current_token.lexeme, interp.pos, interp.line, interp.input_size);
+
     return 0;
 }
