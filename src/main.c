0ddde8de6a70ba5cd9c338cc22c78903872a6c74
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-03-16 22:42:23 +0800

    Add little commentds for the test

--- src/main.c
+++ src/main.c
@@ -11,9 +11,12 @@ int main() {
 
     interp.input = "INT a = 5;";
     interp.input_size = strlen(interp.input);
-    interp.pos = 4;
+    interp.pos = 3;
     interp.line = 1;
 
+    // removed white spaces so the interpreter input will be converted as 
+    // INTa=5;
+    // but this is not the case, we need to split into words as well to become a token
     skip_whitespace(&interp);
     read_identifier(&interp);
 
