a28587589f7009b40c73ca7a37656952c2e66f73
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-03-18 00:01:54 +0800

    Update tokenizer for scanning

--- parser/token.c
+++ parser/token.c
@@ -4,38 +4,39 @@
 #include <ctype.h>
 #include "../include/internal/codecore_token.h"
 
-int CodeToken_OneChar(int c) {
+int CodeOneChar(int c) {
     switch (c) {
-        case '+': return TOKEN_PLUS;
-        case '-': return TOKEN_MINUS;
-        case '*': return TOKEN_ASTERISK;
-        case '/': return TOKEN_SLASH;
-        case '%': return TOKEN_PERCENT;
-        case '(': return TOKEN_LEFT_PAREN;
-        case ')': return TOKEN_RIGHT_PAREN;
-        case '[': return TOKEN_LEFT_BRACKET;
-        case ']': return TOKEN_RIGHT_BRACKET;
-        case '<': return TOKEN_LESS;
-        case '>': return TOKEN_GREATER;
-        case '=': return TOKEN_EQUAL;
-        case '$': return TOKEN_DOLLAR;
-        case '&': return TOKEN_AMPERSAND;
-        case ',': return TOKEN_COMMA;
-        case ':': return TOKEN_COLON;
+        case '+': return PLUS;
+        case '-': return MINUS;
+        case '*': return ASTERISK;
+        case '/': return SLASH;
+        case '%': return PERCENT;
+        case '(': return LEFT_PAREN;
+        case ')': return RIGHT_PAREN;
+        case '[': return LEFT_BRACKET;
+        case ']': return RIGHT_BRACKET;
+        case '<': return LESS;
+        case '>': return GREATER;
+        case '=': return EQUAL;
+        case '$': return DOLLAR;
+        case '&': return AMPERSAND;
+        case ',': return COMMA;
+        case ':': return COLON;
+        default: return INVALID;
     }
 }
 
-int CodeToken_TwoChars(int c1, int c2) {
+int CodeTwoChars(int c1, int c2) {
     switch (c1) {
         case '>':
-            if (c2 == '=') return TOKEN_GREATER_EQUAL;
+            if (c2 == '=') return GREATER_EQUAL;
             break;
         case '<':
-            if (c2 == '=') return TOKEN_LESS_EQUAL;
-            if (c2 == '>') return TOKEN_NOT_EQUAL;
+            if (c2 == '=') return LESS_EQUAL;
+            if (c2 == '>') return NOT_EQUAL;
             break; 
         case '=':
-            if (c2 == '=') return TOKEN_EQUAL_EQUAL;
+            if (c2 == '=') return EQUAL_EQUAL;
             break;
     }
 }
--- parser/tokenizer.c
+++ parser/tokenizer.c
@@ -12,7 +12,7 @@ int is_identifier_char(char c) {
 void advance(Interpreter* interp) {
     interp->pos++;
     if (interp->pos >= interp->input_size) {
-        interp->current_token.type = TOKEN_EOF;
+        interp->current_token.type = EOF;
         interp->current_token.lexeme = NULL;
     }
 }
@@ -22,7 +22,7 @@ void skip_whitespace(Interpreter* interp) {
         char c = interp->input[interp->pos];
         if (isspace(c)) {
             if (c == '\n') {
-                interp->current_token.type = TOKEN_NEWLINE;
+                interp->current_token.type = NEWLINE;
                 interp->current_token.lexeme = NULL;
                 interp->line++;
             }
@@ -49,30 +49,96 @@ void read_identifier(Interpreter* interp) {
     strncpy(identifier, interp->input + start, len);
     identifier[len] = '\0';
 
+    if(CodeOneChar(identifier) != INVALID){
+        interp->current_token.type = CodeOneChar(identifier);
+        return;
+    }
+
     // Check if the identifier is a keyword
     if (strcmp(identifier, "BEGIN") == 0) {
-        interp->current_token.type = TOKEN_BEGIN;
+        interp->current_token.type = BEGIN;
     } else if (strcmp(identifier, "END") == 0) {
-        interp->current_token.type = TOKEN_END;
+        interp->current_token.type = END;
     } else if (strcmp(identifier, "INT") == 0) {
-        interp->current_token.type = TOKEN_INT;
+        interp->current_token.type = INT;
     } else if (strcmp(identifier, "CHAR") == 0) {
-        interp->current_token.type = TOKEN_CHAR;
+        interp->current_token.type = CHAR;
     } else if (strcmp(identifier, "BOOL") == 0) {
-        interp->current_token.type = TOKEN_BOOL;
+        interp->current_token.type = BOOL;
     } else if (strcmp(identifier, "FLOAT") == 0) {
-        interp->current_token.type = TOKEN_FLOAT;
+        interp->current_token.type = FLOAT;
     } else if (strcmp(identifier, "TRUE") == 0) {
-        interp->current_token.type = TOKEN_TRUE;
+        interp->current_token.type = TRUE;
     } else if (strcmp(identifier, "FALSE") == 0) {
-        interp->current_token.type = TOKEN_FALSE;
+        interp->current_token.type = FALSE;
     } else if (strcmp(identifier, "DISPLAY") == 0) {
-        interp->current_token.type = TOKEN_DISPLAY;
+        interp->current_token.type = DISPLAY;
     } else {
-        interp->current_token.type = TOKEN_IDENTIFIER;
+        interp->current_token.type = IDENTIFIER;
     }
 
     interp->current_token.lexeme = identifier;
 }
 
+void read_number(Interpreter* interp) {
+    int start = interp->pos;
+    while (interp->pos < interp->input_size && isdigit(interp->input[interp->pos])) {
+        advance(interp);
+    }
 
+    int len = interp->pos - start;
+    char* number = malloc(len + 1);
+
+    strncpy(number, interp->input + start, len);
+    number[len] = '\0';
+
+    interp->current_token.type = INTEGER;
+    interp->current_token.lexeme = number;
+}
+
+void next_token(Interpreter* interp) {
+    skip_whitespace(interp);
+    if (interp->pos >= interp->input_size) {
+        interp->current_token.type = EOF;
+        interp->current_token.lexeme = NULL;
+        return;
+    }
+
+    char c = interp->input[interp->pos];
+    switch (c) {
+        case '+':
+            interp->current_token.type = PLUS;
+            interp->current_token.lexeme = NULL;
+            advance(interp);
+            break;
+        case '-':
+            interp->current_token.type = MINUS;
+            interp->current_token.lexeme = NULL;
+            advance(interp);
+            break;
+        case '*':
+            interp->current_token.type = ASTERISK;
+            interp->current_token.lexeme = NULL;
+            advance(interp);
+        break;
+        case '/':
+            interp->current_token.type = SLASH;
+            interp->current_token.lexeme = NULL;
+            advance(interp);
+        break;
+        case '%':
+            interp->current_token.type = PERCENT;
+            interp->current_token.lexeme = NULL;
+            advance(interp);
+            break;
+        case '=':
+            if (interp->pos < interp->input_size && interp->input[interp->pos] == '=') {
+                interp->current_token.type = EQUAL;
+                interp->current_token.lexeme = "Assign";
+                interp->pos += 2;
+            } else {
+                error("Invalid token");
+            }
+            break;
+    }
+}
