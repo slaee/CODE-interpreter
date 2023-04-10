d34163421ce7172672ef6627a9e9789d7e76edb4
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-04-10 23:29:56 +0800

    Update project for debugging

--- src/main.c
+++ src/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "rfile.h"
+#include "io.h"
 #include "parser/lexer.h"
 
 int main() {
     extern char* buffer;
-    READ_FILE("test/test.code", BUFFER_SIZE);
+    READ_FILE("test/test.code");
 
     // create lexer
-    lexer* lex = init_code_lexer(buffer, strlen(buffer));
+    Lexer* lex = init_code_lexer(buffer, strlen(buffer));
 
     Token* token;
     while((token = lex_next_token(lex)) && (token->type != TOKEN_EOF)) {
        print_token(token);
     }
- 
+
     return 0;
 }
